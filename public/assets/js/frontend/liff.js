0ff7f9601228fa6ac2f94e6c48a234a77cdf5852
Chao <inm20901@yahoo.com.tw>
2024-07-02 23:04:12 +0800

    no message

--- public/assets/js/frontend/liff.js
+++ public/assets/js/frontend/liff.js
@@ -8,29 +8,26 @@ define(['jquery', 'bootstrap', 'frontend', 'form', 'template'], function ($, und
     };
     var Controller = {
         index: function () {
+            $(document).on("click", ".cho .chess_row a", function () {
+                $(this).addClass("active");
+                let cho = $(this).data("id");
+
+                let cho_chess = $("#cho_chess").val();
+                let choChessArray = cho_chess.split(',');
+                choChessArray.push(cho);
+                $("#cho_chess").val(choChessArray.join(',')); 
+
+                const ids = ['cho1', 'cho2', 'cho3', 'cho4', 'cho5'];
+                ids.forEach((id, index) => {
+                    $("#"+id).text(choChessArray[index]);
+                });
+            });
+
             var mload = layer.load();
             let liff_id = '1655633839-6Jmn4YaZ';
             liff.init({liffId: liff_id}).then(() => {
-                // setTimeout(function () {
-                //     if($('#step0_box')){
-                //         $('#step0_box_loding').hide();
-                //         $('#step0_box').slideDown();
-                //     }
-                // },5000);
                 var liffContext = liff.getContext();
                 if(liffContext.userId){
-                    // $('#userid').val(liffContext.userId);
-                    // $('#step01_form_box_loding').hide();
-                    // $('#step01_form_box').slideDown();
-
-                    
-                    // let options = {
-                    //     url: Config.url.api+'/customer/qrlog?uid='+liffContext.userId
-                    // };
-                    // $.ajax(options);
-
-                    // Layer.msg(liffContext.userId);
-                    
                     $(document).on("click", "#test", function () {
                         liff.sendMessages([
                             {
